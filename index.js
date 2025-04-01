587a58301b430b7d2374a095447fa71664e423b4
Faruqoloyede <oloyedefaruq2@gmail.com>
2025-04-01 08:28:50 +0100

    initial

---
+++ index.js
@@ -0,0 +1,7 @@
+// const generateeRandomnumber = require('./utils');
+
+// console.log(generateeRandomnumber())
+
+import { getPosts } from "./postController.js";
+
+console.log(getPosts())
---
+++ utils.js
@@ -0,0 +1,5 @@
+function generateeRandomnumber (){
+    return Math.floor(Math.random() * 10) + 2
+}
+
+module.exports = generateeRandomnumber;
