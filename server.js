e340d574dd83004a13300dc2bc12106dd76828d3
Faruqoloyede <oloyedefaruq2@gmail.com>
2025-04-02 16:19:31 +0100

    routing

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
-import http from 'http'
+import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
 
 const PORT = process.env.PORT;
-// http createing server
 
-const server = http.createServer((req, res)=>{
-    // passing content type
-    res.writeHead(200, {'content-type': 'text/html'});
-    res.end('<h1>Hello World</h1>'); 
+// get curent path
+
+const _filename = url.fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
+
+console.log(_filename, _dirname)
+
+const server = http.createServer(async (req, res)=>{
+    try {
+        if(req.method === 'GET'){
+            let filePath;
+            if(req.url === '/'){
+               filePath = path.join(_dirname, 'public', 'index.html')
+            }else if(req.url === '/about'){
+                filePath = path.join(_dirname, 'public', 'about.html')
+           }else{
+               throw new Error
+           }
+           
+           const data = await fs.readFile(filePath);
+           res.setHeader('Content-Type', 'text/html');
+           res.write(data);
+           res.end();
+        }else {
+            throw new Error('Method not allowed');
+        }
+    } catch (error) {
+        res.writeHead(500, {'content-type': 'text/plain'});
+        res.end('server Error'); 
+    }
+     
 });
 
 server.listen(PORT, ()=> console.log(`server running on port ${PORT}`));
