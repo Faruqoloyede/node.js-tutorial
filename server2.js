7ad993ad4bb1120edbabd7438876853fa13fd680
Faruqoloyede <oloyedefaruq2@gmail.com>
2025-04-04 09:08:47 +0100

    get user by id (api)

--- server2.js
+++ server2.js
@@ -21,8 +21,29 @@ const server = createServer((req, res)=>{
         res.setHeader('Content-Type', 'application/json');
         res.write(JSON.stringify(users));
         res.end()
-    }else{
-        throw new Error('Method not allowed')
+    }else if(req.url.match(/\api\/users\/([0-9]+)/) && req.method === 'GET'){
+        const id = req.url.split('/')[3];
+        const user = users.find(user => user.id === parseInt(id));
+        if(user){
+            res.setHeader('Content-Type', 'application/json');
+            res.write(JSON.stringify(user));
+            res.end();
+        }else {
+            res.setHeader('Content-Type', 'application/json');
+            res.statusCode = 404;
+            res.write(JSON.stringify({
+             messagge: 'user not found not found'
+            }));
+            res.end();
+        }
+    }
+    else{
+       res.setHeader('Content-Type', 'application/json');
+       res.statusCode = 404;
+       res.write(JSON.stringify({
+        messagge: 'Route not found'
+       }));
+       res.end();
     }
 });
 server.listen(PORT, ()=> console.log(`server running on port ${PORT}`));
